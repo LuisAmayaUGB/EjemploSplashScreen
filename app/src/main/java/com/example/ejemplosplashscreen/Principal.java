3b29ff0c96c10f6e2666d0ba45b6ef7185789323
luis antonio amaya villalta <amayantonio@hotmail.com>
2022-02-26 04:38:47 -0600

    sencillo ejemplo de cómo implementar una pantalla de bienvenida en Android

---
+++ app/src/main/java/com/example/ejemplosplashscreen/Principal.java
@@ -0,0 +1,20 @@
+package com.example.ejemplosplashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+
+
+
+
+    }
+}
